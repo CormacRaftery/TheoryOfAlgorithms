3fd9b5dba66af811fc2e43fa165348680267b4a6
CormacRaftery <G00348802@gmit.ie>
2020-04-09 10:00:30 +0000

    for loop done

--- md5.c
+++ md5.c
@@ -60,10 +60,47 @@ void md5(uint8_t *initial_msg, size_t initial_len) {
 
 	//pre-processing done
 	int offset;
-	for (offset = 0; offset < new_len; offset += (512 / 8)) {
+	for (offset = 0; offset < new_len; offset += 64) {
 
 		//seperate into 16 32-bit words
 		uint32_t *w = (uint32_t*) (msg + offset);
+
+		//Initialize hash values
+		uint32_t a = h0;
+		uint32_t b = h1;
+		uint32_t c = h2;
+		uint32_t d = h3;
+
+		uint32_t i;
+		for (i = 0; i < 64; i++) {
+			uint32_t f, g;
+
+			if (i < 16) {
+				f = (b & c) | ((~b) & d);
+				g = i;
+			} else if (i < 32) {
+				f = (d & b) | ((~d) & c);
+				g = (5 * i + 1) % 16;
+			} else if (i < 48) {
+				f = b ^ c ^ d;
+				g = (3 * i + 5) % 16;
+			} else {
+				f = c ^ (b | (~d));
+				g = (7 * i) % 16;
+			}
+
+			uint32_t temp = d;
+			d = c;
+			c = b;
+			b = b + LEFTROTATE((a + f + k[i] + w[g]), r[i]);
+			a = temp;
+
+		}
+		//add the hash for this 64 bit
+		h0 += a;
+		h1 += b;
+		h2 += c;
+		h3 += d;
 	}
 }
 
